8c0ee8603e6de0e9fd8be99407884ffb602a11e4
Ronja Kääriäinen <Ronja Kriinen>
2017-02-02 17:14:59 +0200

    Changing the env again so this time it should work on production too

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,9 +1,6 @@
 var merge = require('webpack-merge')
 var prodEnv = require('./prod.env')
-var mapsApiKey = require('./mapsApi.env')
 
-module.exports = merge(merge(prodEnv, {
-  		NODE_ENV: '"development"'
-	}), mapsApiKey
-)
-
+module.exports = merge(prodEnv, {
+  	NODE_ENV: '"development"'
+})
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,3 +1,6 @@
-module.exports = {
-  NODE_ENV: '"production"'
-}
+var merge = require('webpack-merge')
+var mapsApiKey = require('./mapsApi.env')
+
+module.exports = merge(mapsApiKey, {
+  	NODE_ENV: '"production"'
+})
